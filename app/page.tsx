cab25598e0f063ef68ce79a5c525a4a696a5d08a
Subham2942 <subham0464@gmail.com>
2025-03-31 00:02:43 +0530

    changed project structure

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Navbar from './components/Navbar'
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Hello, I am Subham Das</h1>
+        <Navbar/>
+      </main>
+    </div>
+  );
+}
